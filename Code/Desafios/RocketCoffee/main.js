803c45f2aa489c1a84d684c1054aa906c1ee7243
Gustavo Almeida <73246160+gustavoalmei@users.noreply.github.com>
2022-03-31 12:00:48 -0300

    coffee

---
+++ Code/Desafios/RocketCoffee/main.js
@@ -0,0 +1,14 @@
+let btt = document.querySelector("#btt")
+let panelbtt = document.querySelector("#panelbtt")
+let panel = document.querySelector("#panel")
+let img = document.querySelector("#imgCheck")
+
+btt.addEventListener("click", ()=>{
+    panel.classList.toggle("uCoffee-open")
+    panel.classList.toggle("uCoffee")
+})
+
+panelbtt.addEventListener("click", ()=>{
+    panel.classList.toggle("uCoffee-open")
+    panel.classList.toggle("uCoffee")
+})
